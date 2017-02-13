347db9f3933ce0729b33fd656e76b019ba99b414
Endy Muhardin <endy.muhardin@gmail.com>
2017-02-13 14:34:38 +0700

    implement routing

--- tabungan-emas/src/app/app.module.ts
+++ tabungan-emas/src/app/app.module.ts
@@ -2,21 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AboutComponent } from './about/about.component';
+
+const routingAplikasi: Routes = [
+	{ path: "about", component: AboutComponent },
+	{ path: "**", component: WelcomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    WelcomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+	RouterModule.forRoot(routingAplikasi)
   ],
   providers: [],
   bootstrap: [AppComponent]
