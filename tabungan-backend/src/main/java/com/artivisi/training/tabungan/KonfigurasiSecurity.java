6001c995887c9659ba0282f9f96568fdbb2fc5a6
Endy Muhardin <endy.muhardin@gmail.com>
2017-02-16 17:13:28 +0700

    implement jwt di resource server

--- tabungan-backend/src/main/java/com/artivisi/training/tabungan/KonfigurasiSecurity.java
+++ tabungan-backend/src/main/java/com/artivisi/training/tabungan/KonfigurasiSecurity.java
@@ -4,8 +4,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
 
 @EnableWebSecurity(debug = true)
 @EnableResourceServer
@@ -14,15 +12,6 @@ public class KonfigurasiSecurity extends ResourceServerConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().anyRequest().authenticated();
-    }
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        RemoteTokenServices remoteToken = new RemoteTokenServices();
-        remoteToken.setClientId("tabunganapp");
-        remoteToken.setClientSecret("app123456");
-        remoteToken.setCheckTokenEndpointUrl("http://localhost:10000/oauth/check_token");
-        resources.tokenServices(remoteToken);
     }
     
 }
