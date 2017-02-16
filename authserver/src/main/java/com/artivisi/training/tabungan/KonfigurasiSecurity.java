edee2ac593fef6b097717d9e396b3c15295492cb
Endy Muhardin <endy.muhardin@gmail.com>
2017-02-16 13:55:44 +0700

    enable debug

--- authserver/src/main/java/com/artivisi/training/tabungan/KonfigurasiSecurity.java
+++ authserver/src/main/java/com/artivisi/training/tabungan/KonfigurasiSecurity.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@EnableWebSecurity
+@EnableWebSecurity(debug = true)
 public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
 
     @Autowired private DataSource dataSource;
