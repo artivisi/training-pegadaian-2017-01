54b7f41ea7e7bccee7d36ec8109e59d534bbe14e
Endy Muhardin <endy.muhardin@gmail.com>
2017-02-16 16:54:34 +0700

    implement jwt di authserver

--- authserver/src/main/java/com/artivisi/training/tabungan/KonfigurasiOAuth2.java
+++ authserver/src/main/java/com/artivisi/training/tabungan/KonfigurasiOAuth2.java
@@ -1,11 +1,16 @@
 package com.artivisi.training.tabungan;
 
+import java.security.KeyPair;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 public class KonfigurasiOAuth2 {
@@ -26,10 +31,24 @@ public class KonfigurasiOAuth2 {
 
         @Override
         public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-            security.checkTokenAccess("hasAuthority('OAUTH_CLIENT')");
+            security.checkTokenAccess("hasAuthority('OAUTH_CLIENT')")
+                    .tokenKeyAccess("permitAll()");
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.accessTokenConverter(jwtAccessTokenConverter());
         }
         
-        
+        @Bean
+        public JwtAccessTokenConverter jwtAccessTokenConverter() {
+            JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+            KeyPair keyPair = new KeyStoreKeyFactory(
+                    new ClassPathResource("keystoretabungan.jks"), "keystore123".toCharArray())
+                    .getKeyPair("tabungan");
+            converter.setKeyPair(keyPair);
+            return converter;
+        }
         
     }
 }
